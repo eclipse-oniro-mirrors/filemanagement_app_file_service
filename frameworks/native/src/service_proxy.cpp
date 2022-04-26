1920bfbd61730c393d68c515e577e1e7f75efb0d
maokelong95 <chenjinglong1@huawei.com>
2022-04-27 00:35:34 +0800

    Refactor SA's proxy to fix iface_cast nullptr issue Change-Id: I627f6c98d15eeb95ddcab3feae3dca575a30e637 Signed-off-by: maokelong95 <chenjinglong1@huawei.com>

--- frameworks/native/src/service_proxy.cpp
+++ frameworks/native/src/service_proxy.cpp
@@ -4,6 +4,8 @@
 
 #include "service_proxy.h"
 #include "filemgmt_libhilog.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
 
 namespace OHOS {
 namespace FileManagement {
@@ -50,6 +52,30 @@ int32_t ServiceProxy::GetFd()
 
     int fd = reply.ReadFileDescriptor();
     return fd;
+}
+
+sptr<IService> ServiceProxy::GetInstance()
+{
+    auto samgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (!samgr) {
+        HILOGE("Get an empty samgr");
+        return nullptr;
+    }
+
+    auto remote = samgr->GetSystemAbility(FILEMANAGEMENT_BACKUP_SERVICE_SA_ID);
+    if (!remote) {
+        HILOGE("Get an empty backup sa");
+        return nullptr;
+    }
+
+    auto proxy = iface_cast<Backup::IService>(remote);
+    if (!proxy) {
+        HILOGE("Get an empty backup sa proxy");
+        return nullptr;
+    }
+
+    // 无需缓存 Proxy，因为 SAMgr 可能因为服务死亡等原因返回不同指针
+    return proxy;
 }
 } // namespace Backup
 } // namespace FileManagement
