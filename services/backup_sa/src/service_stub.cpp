ea680b91ddb9a0e71e9fcd12837baf915c1cc171
maokelong95 <chenjinglong1@huawei.com>
2022-05-11 09:56:27 +0800

    delete extra blank lines Change-Id: Id7f76a2a21267cf5994ce6dc6476e749a198ca2c Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- services/backup_sa/src/service_stub.cpp
+++ services/backup_sa/src/service_stub.cpp
@@ -106,9 +106,7 @@ int32_t ServiceStub::CmdInitBackupSession(MessageParcel &data, MessageParcel &re
 int32_t ServiceStub::CmdGetLocalCapabilities(MessageParcel &data, MessageParcel &reply)
 {
     HILOGE("Begin");
-
     int fd = GetLocalCapabilities();
-
     if (!reply.WriteFileDescriptor(fd)) {
         return BError(BError::Codes::SA_BROKEN_IPC, "Failed to send out the file");
     }
