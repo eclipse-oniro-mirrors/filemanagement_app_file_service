884e7aac2b4ec470f50891626bad52e4478c6b06
maokelong95 <chenjinglong1@huawei.com>
2022-05-18 18:05:24 +0800

    repo upload . 备份部件模块增加读取包含、排除目录功能 Change-Id: Ifa4fe74928f9cfb0b79eff69c706f095cbe72b1c Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

---
+++ utils/src/b_json/b_json_entity_usr_config.cpp
@@ -0,0 +1,64 @@
+/*
+ * 版权所有 (c) 华为技术有限公司 2022
+ */
+
+#include "b_json/b_json_entity_usr_config.h"
+#include "filemgmt_libhilog.h"
+
+namespace OHOS::FileManagement::Backup {
+using namespace std;
+
+BJsonEntityUsrConfig::BJsonEntityUsrConfig(Json::Value &obj) : obj_(obj) {}
+
+vector<string> BJsonEntityUsrConfig::GetIncludeDirs()
+{
+    if (!obj_) {
+        HILOGE("Uninitialized JSon Object reference");
+        return {};
+    }
+    if (!obj_.isMember("includeDirs")) {
+        HILOGE("'includeDirs' field not found");
+        return {};
+    }
+    if (!obj_["includeDirs"].isArray()) {
+        HILOGE("'includeDirs' field must be an array");
+        return {};
+    }
+
+    vector<string> dirs;
+    for (auto &&item : obj_["includeDirs"]) {
+        if (!item.isString()) {
+            HILOGE("Each item of array 'includeDirs' must be of the type string");
+            return {};
+        }
+        dirs.push_back(item.asString());
+    }
+    return dirs;
+}
+
+vector<string> BJsonEntityUsrConfig::GetExcludeDirs()
+{
+    if (!obj_) {
+        HILOGE("Uninitialized JSon Object reference");
+        return {};
+    }
+    if (!obj_.isMember("excludeDirs")) {
+        HILOGE("'excludeDirs' field not found");
+        return {};
+    }
+    if (!obj_["excludeDirs"].isArray()) {
+        HILOGE("'excludeDirs' field must be an array");
+        return {};
+    }
+
+    vector<string> dirs;
+    for (auto &&item : obj_["excludeDirs"]) {
+        if (!item.isString()) {
+            HILOGE("Each item of array 'excludeDirs' must be of the type string");
+            return {};
+        }
+        dirs.push_back(item.asString());
+    }
+    return dirs;
+}
+} // namespace OHOS::FileManagement::Backup
