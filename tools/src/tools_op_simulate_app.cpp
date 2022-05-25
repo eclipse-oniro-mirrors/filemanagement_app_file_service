80d8b6ed72e1cb49ea6233da6d86392a1ecc3fed
huaqingsimeng <1004904143@qq.com>
2022-05-25 16:02:27 +0800

    备份部件修正打包时排除的目录不起作用的问题,增加模拟app打包解包工具 Change-Id: I99efc5c7f4dc2a3887803dff634db417f2c5b08c Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

---
+++ tools/src/tools_op_simulate_app.cpp
@@ -0,0 +1,82 @@
+/*
+ * 版权所有 (c) 华为技术有限公司 2022
+ */
+
+#include <cstdio>
+#include <fcntl.h>
+#include <string_view>
+#include <vector>
+
+#include "b_json/b_json_cached_entity.h"
+#include "b_json/b_json_entity_usr_config.h"
+#include "b_tarball/b_tarball_factory.h"
+#include "tools_op.h"
+
+namespace OHOS::FileManagement::Backup {
+using namespace std;
+
+static string GenHelpMsg()
+{
+    return "This operation helps to simulate the behaviour of a app.\n"
+           "The command format is as follows :\n"
+           "backup_tool simulate app tar user_config.json target.tar root_path\n"
+           "backup_tool simulate app untar target.tar root_path";
+}
+
+void Tar(ToolsOp::CRefVStrView args)
+{
+    string_view fileName = args[0];
+    string_view tarName = args[1];
+    string_view root = args[2];
+
+    BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(fileName.data(), O_RDONLY, 0)));
+    auto cache = cachedEntity.Structuralize();
+    vector<string> incDirs = cache.GetIncludeDirs();
+    vector<string> excDirs = cache.GetExcludeDirs();
+
+    auto tarballFunc = BTarballFactory::Create("cmdline", tarName);
+    (tarballFunc->tar)(root, {incDirs.begin(), incDirs.end()}, {excDirs.begin(), excDirs.end()});
+}
+
+void Untar(ToolsOp::CRefVStrView args)
+{
+    string_view tarName = args[0];
+    string_view root = args[1];
+
+    auto tarballFunc = BTarballFactory::Create("cmdline", tarName);
+    (tarballFunc->untar)(root);
+}
+
+static int Exec(ToolsOp::CRefVStrView args)
+{
+    try {
+        if (args.size() == 4 && args[0] == "tar") {
+            vector<string_view> argsWithoutHead(args.begin() + 1, args.end());
+            Tar(argsWithoutHead);
+        } else if (args.size() == 3 && args[0] == "untar") {
+            vector<string_view> argsWithoutHead(args.begin() + 1, args.end());
+            Untar(argsWithoutHead);
+        } else {
+            printf("Invalid arguments");
+            return -EINVAL;
+        }
+        return 0;
+    } catch (const BError &e) {
+        return e.GetCode();
+    } catch (const exception &e) {
+        HILOGE("Catched an unexpected low-level exception %s", e.what());
+        return -EPERM;
+    }
+}
+
+/**
+ * @brief The hack behind is that "variable with static storage duration has initialization or a destructor with
+ * side effects; it shall not be eliminated even if it appears to be unused" -- point 2.[basic.stc.static].c++ draft
+ *
+ */
+static bool g_autoRegHack = ToolsOp::Register(ToolsOp::Descriptor {
+    .opName = {"simulate", "app"},
+    .funcGenHelpMsg = GenHelpMsg,
+    .funcExec = Exec,
+});
+} // namespace OHOS::FileManagement::Backup
