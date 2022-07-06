5dc02e336d96dc8f3715c45c160744661cac75d4
huaqingsimeng <1004904143@qq.com>
2022-07-06 10:44:15 +0800

    添加测试用例 Change-Id: I1b4a5093869412d6167793645cb0531aadf0839e Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

---
+++ tests/unittests/backup_utils/b_json/b_json_entity_usr_config_test.cpp
@@ -0,0 +1,298 @@
+/*
+ * 版权所有 (c) 华为技术有限公司 2022
+ */
+
+#include <cstdio>
+#include <fcntl.h>
+#include <gtest/gtest.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "b_json/b_json_cached_entity.h"
+#include "b_json/b_json_entity_caps.h"
+#include "b_json/b_json_entity_usr_config.h"
+#include "directory_ex.h"
+#include "file_ex.h"
+#include "test_manager.h"
+
+namespace OHOS::FileManagement::Backup {
+using namespace std;
+
+class BJsonEntityUsrConfigTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {};
+    static void TearDownTestCase() {};
+    void SetUp() {};
+    void TearDown() {};
+};
+
+static vector<string> DEFAULT_INCLUDE_DIR = {
+    "data/storage/el2/database/",
+    "data/storage/el2/base/files/",
+    "data/storage/el2/base/preferences/",
+    "data/storage/el2/base/haps/*/database/",
+    "data/storage/el2/base/haps/*/base/files/",
+    "data/storage/el2/base/haps/*/base/preferences/",
+};
+static vector<string> DEFAULT_EXCLUDE_DIR = {};
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0100
+ * @tc.name: b_json_entity_usr_config_0100
+ * @tc.desc: 不包含includeDirs和excludeDirs
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 0
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0100, testing::ext::TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0100";
+    try {
+        TestManager tm("b_json_entity_usr_config_0100");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, "");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        EXPECT_EQ(incDirs, DEFAULT_INCLUDE_DIR);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        EXPECT_EQ(excDirs, DEFAULT_EXCLUDE_DIR);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0100";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0200
+ * @tc.name: b_json_entity_usr_config_0200
+ * @tc.desc: json文件中只包含includeDirs
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 0
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0200, testing::ext::TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0200";
+    try {
+        TestManager tm("b_json_entity_usr_config_0200");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"includeDirs":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        vector<string> incDirsExpect = {"", "", ""};
+        EXPECT_EQ(incDirs, incDirsExpect);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0200";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0300
+ * @tc.name: b_json_entity_usr_config_0300
+ * @tc.desc: json文件中只包含excludeDirs
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 0
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0300, testing::ext::TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0300";
+    try {
+        TestManager tm("b_json_entity_usr_config_0300");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"excludeDirs":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> excDirs = cache.GetExcludeDirs();
+        vector<string> excDirsExpect = {"", "", ""};
+        EXPECT_EQ(excDirs, excDirsExpect);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0300";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0400
+ * @tc.name: b_json_entity_usr_config_0400
+ * @tc.desc: 同时包含includeDirs和excludeDirs
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 0
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0400, testing::ext::TestSize.Level0)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0400";
+    try {
+        TestManager tm("b_json_entity_usr_config_0400");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"includeDirs":["", "", ""], "excludeDirs":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        vector<string> incDirsExpect = {"", "", ""};
+        EXPECT_EQ(incDirs, incDirsExpect);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        vector<string> excDirsExpect = {"", "", ""};
+        EXPECT_EQ(excDirs, excDirsExpect);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0400";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0500
+ * @tc.name: b_json_entity_usr_config_0500
+ * @tc.desc: json文件中标签为特殊字符
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 2
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0500, testing::ext::TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0500";
+    try {
+        TestManager tm("b_json_entity_usr_config_0500");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"%#$%445":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        vector<string> incDirsExpect = {DEFAULT_INCLUDE_DIR};
+        EXPECT_EQ(incDirs, incDirsExpect);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        vector<string> excDirsExpect = {DEFAULT_EXCLUDE_DIR};
+        EXPECT_EQ(excDirs, excDirsExpect);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0500";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0600
+ * @tc.name: b_json_entity_usr_config_0600
+ * @tc.desc: json文件中标签为中文汉字
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 2
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0600, testing::ext::TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0600";
+    try {
+        TestManager tm("b_json_entity_usr_config_0600");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"测试代码":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        EXPECT_EQ(incDirs, DEFAULT_INCLUDE_DIR);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        EXPECT_EQ(excDirs, DEFAULT_EXCLUDE_DIR);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0600";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0700
+ * @tc.name: b_json_entity_usr_config_0700
+ * @tc.desc: json文件中无标签
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 2
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0700, testing::ext::TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0700";
+    try {
+        TestManager tm("b_json_entity_usr_config_0700");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(pathConfigFile, R"({"":["", "", ""]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        EXPECT_EQ(incDirs, DEFAULT_INCLUDE_DIR);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        EXPECT_EQ(excDirs, DEFAULT_EXCLUDE_DIR);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0700";
+}
+
+/**
+ * @tc.number: SUB_backup_b_json_entity_usr_config_0800
+ * @tc.name: b_json_entity_usr_config_0800
+ * @tc.desc: json文件标签中有异常引号
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 2
+ * @tc.require: AR000H09VQ
+ */
+HWTEST_F(BJsonEntityUsrConfigTest, b_json_entity_usr_config_0800, testing::ext::TestSize.Level2)
+{
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-begin b_json_entity_usr_config_0800";
+    try {
+        TestManager tm("b_json_entity_usr_config_0800");
+
+        string root = tm.GetRootDirCurTest();
+        string pathConfigFile = root + "config.json";
+        SaveStringToFile(
+            pathConfigFile,
+            R"({"includeDirs":["", "", ""], "excludeDirs":["", "", ""], """""""""""""""""""""""""""""""""""""""""""""""""":[]})");
+
+        BJsonCachedEntity<BJsonEntityUsrConfig> cachedEntity(UniqueFd(open(pathConfigFile.data(), O_RDONLY, 0)));
+        auto cache = cachedEntity.Structuralize();
+        vector<string> incDirs = cache.GetIncludeDirs();
+        EXPECT_EQ(incDirs, DEFAULT_INCLUDE_DIR);
+        vector<string> excDirs = cache.GetExcludeDirs();
+        EXPECT_EQ(excDirs, DEFAULT_EXCLUDE_DIR);
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "BJsonEntityUsrConfigTest-end b_json_entity_usr_config_0800";
+}
+} // namespace OHOS::FileManagement::Backup
