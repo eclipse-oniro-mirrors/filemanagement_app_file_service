3a47b4c81146ce6969f345bb2f472711b51805ed
maokelong95 <chenjinglong1@huawei.com>
2022-07-07 15:34:25 +0800

    放在.GN中的、编译期给定的常量宏全面替换到常量定义头文件中 Change-Id: Icea7e21dd30dcfed1807bda4cf3e2175d81c65c5 Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- utils/include/b_resources/b_constants.h
+++ utils/include/b_resources/b_constants.h
@@ -23,6 +23,8 @@ constexpr int SYSTEM_UID = 1000;
 // 应用备份数据暂存路径。
 static inline std::string_view SA_BUNDLE_BACKUP_DIR = "/data/service/el2/100/backup/bundles/";
 static inline std::string_view SA_BUNDLE_BACKUP_BAKCUP = "/backup/";
+static inline std::string_view SA_BUNDLE_BACKUP_ROOT_DIR = "/data/service/el2/100/backup/backup_sa/";
+static inline std::string_view SA_BUNDLE_BACKUP_TMP_DIR = "tmp/";
 static inline std::string_view BACKUP_TOOL_RECEIVE_DIR = "/data/backup/received/";
 static inline std::string_view PATH_BUNDLE_BACKUP_HOME = "/data/storage/el2/backup";
 
