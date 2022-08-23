024aea423658baf84af6dc64cc5d607cf2564e15
huaqingsimeng <1004904143@qq.com>
2022-08-23 15:12:49 +0800

    修复备份服务备份失败后，返回备份成功的问题。 Change-Id: If6865973c18ec8d68966f569c55a0d1e0db17eb7 Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- utils/src/b_tarball/b_tarball_cmdline.cpp
+++ utils/src/b_tarball/b_tarball_cmdline.cpp
@@ -4,6 +4,7 @@
 
 #include "b_tarball/b_tarball_cmdline.h"
 
+#include <sstream>
 #include <string_view>
 #include <unistd.h>
 
@@ -13,6 +14,18 @@
 
 namespace OHOS::FileManagement::Backup {
 using namespace std;
+
+static bool IsTarFatalErrorOccur(string_view output)
+{
+    vector<string_view> fatalError {"EOF",          "bad xform",     "bad header", "sparse overflow",
+                                    "short header", "empty archive", "Not tar"};
+    for (auto &item : fatalError) {
+        if (output.find(item) != string_view::npos) {
+            return true;
+        }
+    }
+    return false;
+}
 
 void BTarballCmdline::Tar(string_view root, vector<string_view> includes, vector<string_view> excludes)
 {
@@ -38,20 +51,23 @@ void BTarballCmdline::Tar(string_view root, vector<string_view> includes, vector
     }
 
     // 如果打包后生成了打包文件，则默认打包器打包时生成的错误可以忽略(比如打包一个不存在的文件)
-    if (int err = BProcess::ExecuteCmd(argv); (err && access(tarballPath_.data(), F_OK) != 0)) {
-        throw BError(BError::Codes::UTILS_INVAL_PROCESS_ARG, to_string(err));
+    auto [bFatalError, errCode] = BProcess::ExecuteCmd(argv, IsTarFatalErrorOccur);
+    if (bFatalError || (errCode && access(tarballPath_.data(), F_OK) != 0)) {
+        stringstream ss;
+        ss << "Is a fatal error occurred: " << bFatalError << ", error code : " << errCode;
+        throw BError(BError::Codes::UTILS_INVAL_PROCESS_ARG, ss.str());
     }
 }
 
 void BTarballCmdline::Untar(string_view root)
 {
-    BProcess::ExecuteCmd({
-        "tar",
-        "-xf",
-        tarballPath_,
-        "-C",
-        root,
-    });
+    vector<string_view> argv = {
+        "tar", "-xf", tarballPath_, "-C", root,
+    };
+    auto [bFatalError, errCode] = BProcess::ExecuteCmd(argv, IsTarFatalErrorOccur);
+    if (bFatalError || errCode) {
+        throw BError(BError::Codes::UTILS_INVAL_PROCESS_ARG, "fatal error occurred in untar process");
+    }
 }
 
 BTarballCmdline::BTarballCmdline(string_view tarballDir, string_view tarballName)
