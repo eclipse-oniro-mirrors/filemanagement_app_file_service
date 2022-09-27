6e456abf2565ec08faa7d473e34b4d787ec8fb2d
huaqingsimeng <1004904143@qq.com>
2022-09-27 14:48:18 +0800

    更新版权信息 Change-Id: I82345dc88bfc74d3f029fbce5553b93e7bf4f932 Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- services/backup_sa/include/module_ipc/svc_session_manager.h
+++ services/backup_sa/include/module_ipc/svc_session_manager.h
@@ -1,5 +1,19 @@
 /*
- * 版权所有 (c) 华为技术有限公司 2022
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * 说明：
  * 备份服务同一时间只接受一个唯一的会话。在会话期间，服务只与一个备份应用通信
  * 且只响应备份或恢复接口中的一种。
--- utils/include/b_process/b_process.h
+++ utils/include/b_process/b_process.h
@@ -1,5 +1,16 @@
 /*
- * 版权所有 (c) 华为技术有限公司 2022
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef OHOS_FILEMGMT_BACKUP_B_PROCESS_H
@@ -33,7 +44,7 @@ public:
      * @throw BError(UTILS_INTERRUPTED_PROCESS) 系统调用异常(pipe调用失败、dup2调用失败、子进程被信号终止)
      */
     static std::tuple<bool, ErrCode> ExecuteCmd(std::vector<std::string_view> argv,
-                                           std::function<bool(std::string_view)> DetectFatalLog = nullptr);
+                                                std::function<bool(std::string_view)> DetectFatalLog = nullptr);
 
 private:
     BProcess() = delete;
