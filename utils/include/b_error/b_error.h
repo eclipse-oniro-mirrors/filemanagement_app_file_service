d2ece3da0e4431dc0912b5cd790d784d8867ddac
maokelong95 <chenjinglong1@huawei.com>
2022-09-27 14:48:18 +0800

    更新版权信息 Change-Id: I82345dc88bfc74d3f029fbce5553b93e7bf4f932 Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- utils/include/b_error/b_error.h
+++ utils/include/b_error/b_error.h
@@ -1,6 +1,19 @@
-/**
- * 版权所有 (c) 华为技术有限公司 2022
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * 本部件处理错误的原则：
  *     原则1：使用异常表示错误，但只有无法处理的问题才算得上是错误，否则只是普通的边界分支；
  *     原则2：仅在模块内部使用异常，而在界面层Catch所有异常，从而防止异常扩散；
