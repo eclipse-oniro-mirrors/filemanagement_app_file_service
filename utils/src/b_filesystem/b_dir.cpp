b42df85450efccc4ebe7305f345edb0e61447577
maokelong95 <chenjinglong1@huawei.com>
2022-10-26 16:01:28 +0800

    backup_config.json文件标签中有匹配的异常括号会导致打包失败的问题 Change-Id: I0e7a14190dccf232aba511505915bcdbc462328d Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

--- utils/src/b_filesystem/b_dir.cpp
+++ utils/src/b_filesystem/b_dir.cpp
@@ -106,8 +106,7 @@ tuple<ErrCode, vector<string>> BDir::GetDirFiles(const string &path)
 set<string> ExpandPathWildcard(const vector<string> &vec)
 {
     unique_ptr<glob_t, function<void(glob_t *)>> gl {new glob_t, [](glob_t *ptr) { globfree(ptr); }};
-    gl->gl_offs = 0;
-    gl->gl_pathc = 0;
+    *gl = {};
 
     int flags = GLOB_DOOFFS | GLOB_MARK;
     for (const string &pattern : vec) {
