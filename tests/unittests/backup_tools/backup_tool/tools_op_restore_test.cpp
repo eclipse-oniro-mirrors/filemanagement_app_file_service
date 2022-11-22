8b34b6e2305ae93905eaaa96ec47169c67292694
huaqingsimeng <1004904143@qq.com>
2022-11-22 09:22:31 +0800

    新增TDD测试用例 Change-Id: Idb667e7e2aadd3919df3af27409e93012d9f75c7 Signed-off-by: huaqingsimeng <qiukaiqing@huawei.com>

---
+++ tests/unittests/backup_tools/backup_tool/tools_op_restore_test.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <map>
+#include <sstream>
+
+#include "tools_op.h"
+
+namespace OHOS::FileManagement::Backup {
+using namespace std;
+class ToolsOpRestoreTest : public testing::Test {
+public:
+    static void SetUpTestCase(void) {};
+    static void TearDownTestCase() {};
+    void SetUp() {};
+    void TearDown() {};
+};
+
+/**
+ * @tc.number: SUB_backup_tools_op_restore_0100
+ * @tc.name: SUB_backup_tools_op_restore_0100
+ * @tc.desc: 测试
+ * @tc.size: MEDIUM
+ * @tc.type: FUNC
+ * @tc.level Level 1
+ * @tc.require: SR000H0382
+ */
+HWTEST_F(ToolsOpRestoreTest, SUB_backup_tools_op_restore_0100, testing::ext::TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "ToolsOpRestoreTest-begin SUB_backup_tools_op_restore_0100";
+    try {
+        GTEST_LOG_(INFO) << "ToolsOpRestoreTest-info";
+        map<string, vector<string>> mapArgToVal;
+        vector<string> path = {"/data/backup/tmp"};
+        mapArgToVal.insert(make_pair("pathCapFile", path));
+        vector<string> bundles = {"com.example.app2backup"};
+        mapArgToVal.insert(make_pair("bundles", bundles));
+
+        // 创建测试路径
+        string strPath = "/data/backup";
+        mkdir(strPath.data(), S_IRWXU);
+
+        // 尝试匹配当前命令，成功后执行
+        GTEST_LOG_(INFO) << "ToolsOpRestoreTest-backup";
+        vector<string_view> curOp;
+        curOp.emplace_back("restore");
+        auto tryOpSucceed = [&curOp](const ToolsOp &op) { return op.TryMatch(curOp); };
+        auto &&opeartions = ToolsOp::GetAllOperations();
+        auto matchedOp = find_if(opeartions.begin(), opeartions.end(), tryOpSucceed);
+        if (matchedOp != opeartions.end()) {
+            matchedOp->Execute(mapArgToVal);
+        }
+    } catch (...) {
+        EXPECT_TRUE(false);
+        GTEST_LOG_(INFO) << "ToolsOpRestoreTest-an exception occurred by construction.";
+    }
+    GTEST_LOG_(INFO) << "ToolsOpRestoreTest-end SUB_backup_tools_op_restore_0100";
+}
+} // namespace OHOS::FileManagement::Backup
